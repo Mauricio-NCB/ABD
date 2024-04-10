cb37e8ec1ef6b602430edda10758f41a52e1f776
Mauricio-NCB <mncalderon2001@gmail.com>
2024-04-10 10:21:19 +0200

    Update Usuario.php

--- ABD/Usuario.php
+++ ABD/Usuario.php
@@ -1,6 +1,11 @@
 <?php
-echo
+$tituloPagina="Página de usuario";
+$contenidoPrincipal="Contenido del usuario";
+require_once('plantilla.php');
+
+/*echo
 "<h1>Bienvenido Usuario</h1>
 <a href='registro.php'>Registro</a><br>
 <a href='login.php'>Login</a>";
+*/
 ?>

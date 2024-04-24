26e457a5b52709c216e6d7fca23ad228313fcd97
Mauricio-NCB <mncalderon2001@gmail.com>
2024-04-24 13:44:56 +0200

    Cambios clase pelicula

--- ABD/require/logica/Pelicula.php
+++ ABD/require/logica/Pelicula.php
@@ -1,5 +1,7 @@
 <?php
 
+use Aplicacion;
+
 //Tabla Pelicula. Hecha para almacenar los datos de cada pelicula que se va a alquilar
 
 class Pelicula {
@@ -13,13 +15,34 @@ class Pelicula {
     private $valoracion;
     private $comentarios;
 
-    private function __construct ($id = null, $nombre, $descripcion, $precio, $valoracion, $comentarios) {
+    private function __construct ($id = null, $nombre, $descripcion, $precio, $valoracion, $comentarios = []) {
         $this->id = $id;
         $this->nombre = $nombre;
         $this->descripcion = $descripcion;
         $this->precio = $precio;
         $this->valoracion = $valoracion;
         $this->comentarios = $comentarios;
+    }
+
+    public static function anadirPelicula($nombre, $descripcion, $precio, $valoracion) {
+        
+        $result = true;
+
+        // Se almacena la nueva pelicula creada
+
+        $pelicula = new Pelicula($nombre, $descripcion, $precio, $valoracion);
+        if (!$pelicula->anadir($pelicula)) {
+            error_log("Error de insercion: No se ha podido insertar de manera correcta la nueva pelicula");
+            $result = false;
+        }
+
+        return $result;
+    }
+
+    private static function anadir($pelicula) {
+        $result = false;
+        $conn = Aplicacion::getInstance()->getConexionBd();
+        $query = sprintf ("INSERT INTO Peliculas(Nombre, Descripcion, Precio, Valoracion)");
     }
 }
 

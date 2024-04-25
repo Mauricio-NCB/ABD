209f41ecc1c85b57d92990b4c2a6e89e33ca6f16
Mauricio-NCB <mncalderon2001@gmail.com>
2024-04-26 01:03:25 +0200

    Funciones pelicula sin terminar

--- ABD/require/logica/Pelicula.php
+++ ABD/require/logica/Pelicula.php
@@ -15,7 +15,7 @@ class Pelicula {
     private $valoracion;
     private $comentarios;
 
-    private function __construct ($id = null, $nombre, $descripcion, $precio, $valoracion, $comentarios = []) {
+    private function __construct ($id = null, $nombre, $descripcion, $precio, $valoracion = null, $comentarios = []) {
         $this->id = $id;
         $this->nombre = $nombre;
         $this->descripcion = $descripcion;
@@ -24,13 +24,13 @@ class Pelicula {
         $this->comentarios = $comentarios;
     }
 
-    public static function anadirPelicula($nombre, $descripcion, $precio, $valoracion) {
+    public static function anadirPelicula($nombre, $descripcion, $precio) {
         
         $result = true;
 
         // Se almacena la nueva pelicula creada
 
-        $pelicula = new Pelicula($nombre, $descripcion, $precio, $valoracion);
+        $pelicula = new Pelicula($nombre, $descripcion, $precio);
         if (!self::anadir($pelicula)) {
             error_log("Error de insercion: No se ha podido insertar de manera correcta la nueva pelicula");
             $result = false;
@@ -42,8 +42,49 @@ class Pelicula {
     private static function anadir($pelicula) {
         $result = false;
         $conn = Aplicacion::getInstance()->getConexionBd();
-        $query = sprintf ("INSERT INTO Peliculas(Nombre, Descripcion, Precio, Valoracion)");
+        $query = sprintf("INSERT INTO pelicula(id, nombre, descripcion, precio)
+                        VALUES ('%s', '%s', '%s', '%s')"
+                    , $conn->real_escape_string($pelicula->nombre)
+                    , $conn->real_escape_string($pelicula->descripcion)
+                    , $conn->real_escape_string($pelicula->precio));
+        
+        if ($conn->query($query)) {
+            $pelicula->id = $conn->insert_id;
+            $result = true;
+        }
+        else {
+            error_log("Error BD ({$conn->errno}): {$conn->error}");
+        }
+        return $result;
     }
+
+    public static function eliminarPelicula($nombre) {
+        return $result = self::eliminar($nombre);
+    }
+
+    private static function eliminar($nombre) {
+        $result = false;
+        $conn = Aplicacion::getInstance()->getConexionBd();
+        $query = sprintf("DELETE * FROM pelicula P WHERE P.nombre = $nombre");
+        if ($conn->query($query)) {
+            $result = true;
+        }
+        else {
+            error_log("Error BD ({$conn->errno}): {$conn->error}");
+        }
+        return $result;
+    }
+
+    static function mostrarPelicula($id) {
+        $pelicula = self::buscarPelicula($id);
+        return $pelicula;
+    }
+
+    static function mostrarCartelera() {
+        $pelicula = self::filtrarPelicula();
+        return $pelicula;
+    }
+
 }
 
 ?>

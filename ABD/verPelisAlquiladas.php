aeb246eed14e8d1136a0e5e09c92fcf553fb5a23
margal15 <margal15@ucm.es>
2024-05-07 21:32:48 +0200

    Muestra valoraciones y comentarios de la película si esta alquilada

--- ABD/verPelisAlquiladas.php
+++ ABD/verPelisAlquiladas.php
@@ -20,10 +20,26 @@ if(count($peliculas) != 0){
             <p>Nombre de la pelicula: {$row['nombre']}</p>
             <p>Descripcion de la pelicula: {$row['descripcion']}</p>
             <p>Precio del alquiler: {$row['precio']}</p> 
-            <p>-------------------------------------</p>
             EOS;
+        $query2 = sprintf("SELECT puntuacion , comentario FROM valoracion WHERE idPelicula = '$row[id]'"); 
+        $result2 = $conn->query($query2);
+        $row2 = $result2->fetch_array();
+        if($result2->num_rows!=0){
+            $contenidoPrincipal .= <<< EOS
+                <p>Valoración sobre la película: {$row2['puntuacion']} </p>
+                <p>Comentario sobre la película: {$row2['comentario']} </p>
+            EOS;
+        }
+        
+        $contenidoPrincipal .= <<< EOS
+        <p>-------------------------------------</p>
+        EOS;
+        
         $i++;
+        
     }
+    $result->free();
+    $result2->free();
 }
 else{
     $contenidoPrincipal .= "No hay ninguna pelicula alquilada.";
